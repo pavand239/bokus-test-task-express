49cc6eba9cd8b01a2f97a5815bdb4f56c4b9147f
Pavlov Andrey <pavand239@gmail.com>
2020-03-15 17:18:18 +0300

    add babel, nodemon

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,25 +1,22 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
-var app = require('../app');
-var debug = require('debug')('bokus-test-task-express:server');
-var http = require('http');
+import app from "../app";
+import debugLib from "debug";
+import http from "http";
+const debug = debugLib('bokus-test-task-express:server');
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
