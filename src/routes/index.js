147f9acc2739a7eea98eac4a9372ad028cac6fb0
Pavlov Andrey <pavand239@gmail.com>
2020-03-16 11:19:08 +0300

    prepare data to render inside renderTable func

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,9 +10,20 @@ const fetchData = async (req, res, next) => {
   req.data = data;
   next();
 };
+
+// функция для рендеринга таблицы
 const renderTable = (req, res) => {
-  let { user, book } = req.data;
-  res.render('index', {user, book});
+  let { user, book } = req.data,
+    users = Object.values(user),// преобразование объекта в массив для использования map
+    books = Object.values(book);
+  // для каждого объекта user создается свойтво - массив книг этого user  с помощью filter
+  users.forEach(user => user.books = books.filter(book => book.uid === user.uid)); 
+  console.log(users);
+  res.render('index', 
+              {
+                title:'Bokus Test Task ExpressJS',
+                users,
+              });
 };
 /* GET home page. */
 router.get('/', 
