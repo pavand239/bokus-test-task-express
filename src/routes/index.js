0742950dcf1dc8dd1f3fbdbee5b50fa3802115e6
Pavlov Andrey <pavand239@gmail.com>
2020-03-16 10:08:24 +0300

    add fetchData function

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import fetch from "node-fetch";
+
 const router = express.Router();
 
+// функция для загрузки данных 
+const fetchData = async (req, res, next) => {
+  let response = await fetch('http://z.bokus.ru/user.json');
+  let data = await response.json();
+  req.data = data;
+  next();
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', fetchData, (req, res, next) => {
+  console.log(req.data);
   res.render('index', { title: 'My first express app' });
 });
 
